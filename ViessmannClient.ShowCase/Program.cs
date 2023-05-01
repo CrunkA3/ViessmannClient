095f791ff3a953db9ba100ad6a240fb84e40a163
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2023-05-01 16:24:53 +0200

    add showcase

---
+++ ViessmannClient.ShowCase/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using ViessmannClient;
+using ViessmannClient.Model.Installations;
+using ViessmannClient.Network;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var auth = new ViessmannAuth();
+var viessmannConnectionProvider = new ViessmannConnectionProvider<ViessmannPlatformClient>(auth)
+{
+    ClientId = config["viessmannClientId"],
+    RedirectUri = "http://localhost:59926/ViessmannLogin"
+};
+
+using var client = new ViessmannPlatformClient(viessmannConnectionProvider);
+
+
+app.MapGet("", () => Results.Redirect(client.GetLoginUri().ToString(), false));
+
+app.MapGet("/ViessmannLogin", async (string code) =>
+{
+    var loginResult = await client.TryCompleteLogin(code);
+    if (!loginResult) return Results.Problem("Login nicht erfolgreich");
+
+    var installations = await client.GetInstallations();
+
+    return Results.Ok(installations);
+})
+    .Produces<IEnumerable<Installation>>();
+
+
+app.Run();
